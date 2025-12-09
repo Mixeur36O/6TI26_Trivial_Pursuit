dbad133c56bafe551179d7d86be623b8c6c853e6
Maxence <200040@site.asty-moulin.be>
2025-12-09 15:32:50 +0100

    09/12/2025

--- Code_Test/Test_1_Plateau/classe/Joueur.cs
+++ Code_Test/Test_1_Plateau/classe/Joueur.cs
@@ -8,5 +8,33 @@ namespace Limet_Maxence_CodagePion.Classe
 {
     internal class Joueur
     {
+        //Attributs
+        private string _pseudo;
+        private int _nbrJoueur;
+
+        //Props
+
+        public string Pseudo
+        {
+            get { return _pseudo; }
+            set { _pseudo = value; }
+        }
+
+        public int NbrJoueur
+        {
+            get { return _nbrJoueur; }
+            set { _nbrJoueur = value; }
+        }
+
+
+        //Construct
+        public Joueur(string pseudo, int nbrJoueur)
+        {
+            _pseudo = pseudo;
+            _nbrJoueur = nbrJoueur;
+        }
+
+        //Méthode
+
     }
 }
