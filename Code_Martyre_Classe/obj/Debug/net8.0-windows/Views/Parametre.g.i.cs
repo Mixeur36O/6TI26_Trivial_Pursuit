48bd55a331b2899b1611bd785ce268758d1782bb
WikiNat <nathan.marcq@icloud.com>
2026-02-02 11:16:47 +0100

    gg

--- Code_Martyre_Classe/obj/Debug/net8.0-windows/Views/Parametre.g.i.cs
+++ Code_Martyre_Classe/obj/Debug/net8.0-windows/Views/Parametre.g.i.cs
@@ -55,13 +55,13 @@ namespace Code_Martyre_Classe.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Code_Martyre_Classe;component/views/parametre.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Code_Martyre_Classe;V1.0.0.0;component/views/parametre.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\Parametre.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +71,7 @@ namespace Code_Martyre_Classe.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
