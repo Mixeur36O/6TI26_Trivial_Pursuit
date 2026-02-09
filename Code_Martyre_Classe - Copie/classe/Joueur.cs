60a2d42ac58a23427e8a98a1513f2cfce127f317
Limet Maxence <200040@CCM-Asty-Moulin.local>
2026-02-09 12:01:01 +0100

    09/02/2026

---
+++ Code_Martyre_Classe - Copie/classe/Joueur.cs
@@ -0,0 +1,40 @@
+﻿using Code_Martyre_Classe.Config;
+using Org.BouncyCastle.Tls.Crypto.Impl.BC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Limet_Maxence_CodagePion.Classe
+{
+    internal class Joueur
+    {
+        //Attributs
+        private string _pseudo;
+
+        //Props
+
+        public string Pseudo
+        {
+            get { return _pseudo; }
+            set { _pseudo = value; }
+        }
+
+
+        //Construct
+        public Joueur(string pseudo)
+        { 
+            _pseudo = pseudo;
+        }
+
+        connectDB bdd = new connectDB();
+        //Méthode
+
+        public void AjoutePseudo()
+        {
+            bdd.AjouteJoueur(_pseudo);
+        }
+
+    }
+}
